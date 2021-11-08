490ea25460be7f8b1c50d200735d3319e09cc978
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2021-11-08 15:31:35 +0600

    api updated

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -5,17 +5,19 @@ import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([])
     useEffect(() => {
-        fetch("https://restcountries.eu/rest/v2/all")
+        // fetch("https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR1nT-SuE7XIJHUlnNmzMRF98HqfNT_ixJIs_t4m5NncMIC5qbAIR-8Rlds")
+        fetch("https://restcountries.com/v3.1/all")
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
     return (
         <div>
             <h4>Countries Available: {countries.length}</h4>
+            {/* <h5>API used: https://restcountries.eu/rest/v2/all</h5> */}
             <div className='countries'>
                 {
                     countries.map(country => <SingleCountry
-                        key={country.name}
+                        key={country.name.common}
                         country={country} />)
                 }
             </div>
--- src/components/Country/SingleCountry.js
+++ src/components/Country/SingleCountry.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './SingleCountry.css'
 
 const SingleCountry = (props) => {
-    const { name, capital, flag, population, region } = props.country
+    const { name, capital, flags, population, region } = props.country
     return (
         <div className="country">
-            <img src={flag} alt="flag" />
-            <h1>{name}</h1>
+            <img src={flags.png} alt="flag" />
+            <h1>{name.common}</h1>
             <p>Capital: {capital} </p>
             <p>Population: {population} </p>
             <p>Region: {region} </p>
